f9ecb572bac7c31c610dfe075f0bfd8d1b6a0a5b
Admin <liseczkij2@gmail.com>
2023-01-27 17:23:09 +0200

    UP

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,12 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Main from "../Main/Main";
-
 import '../assets/fonts/Inconsolata-Black.ttf'
 import './App.css'
+
+
+
+
 function App() {
     return  (
         <div className='container-page'>
